e354dee361f65084f5a2b163c7b3f15ca3e08745
ocean010315 <elena0315@naver.com>
2024-11-06 11:31:50 +0900

    feat: notebook files to python file

---
+++ shyun/fewshots/prompt.py
@@ -0,0 +1,58 @@
+RANDOM = 42
+
+def extract_label_prompt():
+    sys_prompt = '''당신은 기사 제목을 보고 어떤 분야의 기사인지 맞추는 전문가입니다.
+[지시사항]
+1. 주어진 데이터는 개행 기호(\\n)로 구분된 뉴스 기사 제목들입니다.
+2. 주어진 데이터에는 임의의 자리에 문맥에 맞지 않는 글자가 무작위로 삽입되어 있습니다.
+3. 해당 데이터들을 가장 잘 표현하는 포괄적인 기사 분야를 한 단어로만 출력하세요.'''
+
+    fewshot = [
+        {"role": "user", "content": "신간2문학A음악이 q야기ji"},
+        {"role": "assistant", "content": "문화"},
+        {"role": "user", "content": "정부 4월 한반도 위설d근거O다Y되J Q6야d합"},
+        {"role": "assistant", "content": "정치"},
+        {"role": "user", "content": "충북교육청 노h조합mYfR동K군별 4양한 목소OH용"},
+        {"role": "assistant", "content": "사회"},
+        {"role": "user", "content": "국제k호단체HrUTs 유엔 테단체 1정 움직f에k우려"},
+        {"role": "assistant", "content": "해외"}
+    ]
+
+    return sys_prompt, fewshot
+
+def clean_text_promt(key):
+    sys_prompt = f'''당신은 기사 제목을 복원하는 전문가입니다.
+1. 키워드는 '{key}'입니다.
+2. 어색한 문맥을 키워드를 참고하여 올바르게 고치세요.
+3. 복원이 안 되거나 일부만 가능할 경우, 해당 키워드를 포함한 형태로 대체하세요.'''
+
+    fewshot = [
+        {"role": "user", "content": "해외로밍 m금폭탄 n동차단 더 빨진다"},
+        {"role": "assistant", "content": "해외로밍 요금 폭탄 자동차단 더 빨라진다"},
+        {"role": "user", "content": "코z나0i9규직Zj정규직 E문R비교"},
+        {"role": "assistant", "content": f"{key}코로나 정규직 비정규직 E문R비교"},
+        {"role": "user", "content": "정i 파1 미사z KT 이용기간 2e 단 Q분종U2보"},
+        {"role": "assistant", "content": f"{key} 정i 파1 미사z KT 이용기간 2e 단 Q분종U2보"}
+    ]
+
+    return sys_prompt, fewshot
+
+def clean_label_prompt(keys):
+    keys_str = ', '.join(keys)
+
+    sys_prompt = f'''기사 제목을 보고 주어진 분야 중 올바른 분야의 번호를 선택하세요.
+    - 기사 분야: {keys_str}
+    - 위 분야 중 올바른 분야의 번호를 선택하세요.
+    - 문맥적으로 올바르지 않은 데이터는 '불가'라고 출력하세요.
+    - 반드시 주어진 키워드 중 한 개만 선택하세요.'''
+
+    fewshot = [
+        {"role": "user", "content": "구미 영천 등 경북 9개 시군 폭염주의보"},
+        {"role": "assistant", "content" : "0"},
+        {"role": "user", "content": "울산 진보3당 북구시설관리공단 설립 조례안 철회하라"},
+        {"role": "assistant", "content": "2"},
+        {"role": "user", "content": "국제유가곡물가올해 농업경영비 33 감소 전망"},
+        {"role": "assistant", "content": "3"}
+    ]
+
+    return sys_prompt, fewshot
---
+++ shyun/llama.py
@@ -0,0 +1,127 @@
+import os, json, tqdm
+
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+from fewshots import prompt
+
+MODEL = 'Bllossom/llama-3.2-Korean-Bllossom-3B' # 'beomi/Llama-3-Open-Ko-8B'
+
+class Llama:
+    def __init__(self):
+        self.tokenizer = AutoTokenizer.from_pretrained(MODEL)
+        self.model = AutoModelForCausalLM.from_pretrained(
+            MODEL,
+            torch_dtype=torch.float16,
+            device_map='auto'
+        )
+
+        self.model.eval()
+
+        self.terminators = [
+            self.tokenizer.convert_tokens_to_ids("<|end_of_text|>"),
+            self.tokenizer.convert_tokens_to_ids("<|eot_id|>")
+        ]
+
+    def generate(self, message):
+        input_ids = self.tokenizer.apply_chat_template(
+            message,
+            add_generation_prompt=True,
+            return_tensors='pt'
+        ).to(self.model.device)
+
+        outputs = self.model.generate(
+            input_ids,
+            max_new_tokens=1024,
+            eos_token_id=self.terminators,
+            pad_token_id=self.tokenizer.eos_token_id,
+            do_sample=True,
+            temperature=0.6,
+            top_p=0.9
+        )
+
+        return self.tokenizer.decode(outputs[0][input_ids.shape[-1]:], skip_special_tokens=True)
+    
+    def extract_label(self, train_dataset):
+        sys_prompt, fewshot = prompt.extract_label_prompt()
+
+        target = train_dataset[(0.30<train_dataset['polluted_lv']) & (train_dataset['polluted_lv']<0.40)]
+
+        keywords = []
+        for i in range(7):
+            keys = []
+            for j in range(10):
+                data = target[(target['target']==i)].sample(5)['text'].values
+                data = '\n'.join(data)
+
+                messages = \
+                    [{"role": "system", "content": f"{sys_prompt}"}] + \
+                    fewshot + \
+                    [{"role": "user", "content": f"{data}"}]
+                
+                result = self.generate(messages)
+
+                keys.append(result)
+            keywords.append(keys)
+        
+        sys_prompt = '''제공된 데이터들은 뉴스 기사 분야 10개에 대한 단어입니다. 키워드를 추출하세요.
+- 많이 등장하는 단어보다 전체 단어들의 맥락을 고려하세요.
+- 여러 개의 키워드가 있다면 가장 포괄적인 키워드 한 개만 출력하세요.'''
+
+        final_keys = []
+        for keys in keywords:
+            keys = ', '.join(keys)
+            messages = \
+                [{"role": "system", "content": sys_prompt}] + \
+                fewshot +\
+                [{"role": "user", "content": keys}]
+            
+            result = self.generate(messages)
+            final_keys.append(result)
+        
+        key_maps = {}
+        for idx, key in enumerate(final_keys):
+            key_maps[idx] = key
+        
+        with open('./key_maps.json', 'w') as f:
+            json.dump(key_maps, f, ensure_ascii=False)
+
+    def clean_label(self, keys, train_dataset, p, path): # keys: json 형태의 key 그대로 전달
+        sys_prompt, fewshot = prompt.clean_label_prompt(keys)
+
+        target = train_dataset[train_dataset['polluted_lv'] < p]
+
+        cnt = 0
+        for _, data in tqdm(target.iterrows(), desc='labeling', total=len(target)):
+            messages = \
+                [{"role": "system", "content": sys_prompt}] + \
+                fewshot + \
+                [{"role": "user", "content": data['text']}]
+            
+            result = self.generate(messages)
+
+            if result != '불가':
+                cnt += 1
+                train_dataset.loc[train_dataset['ID']==data['ID'], 'target'] = int(result)
+        
+        print(f"총 {cnt}개의 라벨이 다시 설정되었습니다.")
+        train_dataset.to_csv(os.path.join(path, 'label_cleaned.csv'), index=False)
+
+    def clean_text(self, keys, train_dataset, p, path):
+        target = train_dataset[train_dataset['pollute_lv'] > p]
+
+        for idx, key in tqdm(enumerate(keys.values()), total=len(keys), position=0):
+            sys_prompt, fewshot = prompt.clean_text_prompt(key)
+
+            key_target = target[target['target']==idx]
+            for _, data in tqdm(key_target.iterrows(), desc=f"label={idx}", total=len(key_target), position=1, leave=False):
+                messages = \
+                    [{"role": "system", "content": sys_prompt}] + \
+                    fewshot + \
+                    [{"role": "user", "content": data['text']}]
+                
+                result = self.generate(messages)
+                train_dataset.loc[train_dataset['ID']==data['ID'], 'text'] = result
+        
+        train_dataset.to_csv(os.path.join(path, 'text_cleaned.csv'), index=False)
+        
