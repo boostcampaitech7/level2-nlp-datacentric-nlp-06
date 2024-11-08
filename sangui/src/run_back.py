963cb5874487fbda094284bf31213af7bf7f4703
LeSaUi <rmrlrmrl126@gmail.com>
2024-11-08 12:37:26 +0900

    feat: merge train and valid data

--- sangui/src/run_back.py
+++ sangui/src/run_back.py
@@ -52,6 +52,9 @@ torch.cuda.empty_cache()
 print("label split")
 ascii.label_train_valid_split(pivot_name, ascii_ratio)
 
+print("train valid merge")
+ascii.train_valid_merge(pivot_name, ascii_ratio)
+
 # %%
 print("blm")
 blm = BLM(config)
